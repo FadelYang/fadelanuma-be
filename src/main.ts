cf0d89f43058da6f04f596d1fc8ebcc224f44ad2
fadelanumah <fadelanumah@gmail.com>
2025-01-29 16:34:43 +0700

    chore: setup validation config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global pipe that will be applied to all routes
+  // This will validate the request body againts the DTO
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
