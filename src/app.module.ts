3170541b40ce90c7c213d2842f7aeeefd2a9c79b
fadelanumah <fadelanumah@gmail.com>
2025-01-29 17:25:13 +0700

    chore: setup jwt service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './core/services/prisma/prisma.service';
+import { UsersModule } from './modules/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: 'this_is_super_secret_key',
+      signOptions: { expiresIn: '12h' },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
