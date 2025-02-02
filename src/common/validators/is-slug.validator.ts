6e701aa4236e40f8a8fe3f11263905bceac0bb1e
fadelanumah <fadelanumah@gmail.com>
2025-02-02 11:10:33 +0700

    fix(slug-validator): fix if name not requested

--- src/common/validators/is-slug.validator.ts
+++ src/common/validators/is-slug.validator.ts
@@ -1,15 +1,19 @@
-import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
+import {
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
 
-@ValidatorConstraint({name: 'IsSlugVaid', async: false})
+@ValidatorConstraint({ name: 'IsSlugVaid', async: false })
 export class IsSlugVaid implements ValidatorConstraintInterface {
   validate(slug: string, args: ValidationArguments) {
-    const obj = args.object as any
-    if (!onrejectionhandled.name) return false;
-    const expectedSlug = obj.name.toLowerCase().split(' ').join('-')
-    return slug === expectedSlug
+    const obj = args.object as any;
+    if (!obj.name) return false;
+    const expectedSlug = obj.name.toLowerCase().split(' ').join('-');
+    return slug === expectedSlug;
   }
 
   defaultMessage(validationArguments?: ValidationArguments): string {
-    return "Slug must be the lowercase version of name with title/name with spaces replace by '-"
+    return "Slug must be the lowercase version of name with title/name with spaces replace by '-";
   }
 }
