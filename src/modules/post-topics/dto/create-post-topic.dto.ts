0bc68cc7192d0f6f0a691fc7cb674abd60c1079a
fadelanumah <fadelanumah@gmail.com>
2025-02-02 23:45:51 +0700

    docs: add detail api property in post topic and type resource

--- src/modules/post-topics/dto/create-post-topic.dto.ts
+++ src/modules/post-topics/dto/create-post-topic.dto.ts
@@ -1,14 +1,29 @@
-import { Type } from 'class-transformer';
-import { IsDate, IsInt, IsNotEmpty, Validate } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, Validate } from 'class-validator';
 import { IsSlugVaid } from 'src/common/validators/is-slug.validator';
 
 export class CreatePostTopicDto {
+  @ApiProperty({
+    example: 'Web Development',
+    description: 'The name of post topic',
+    type: String
+  })
   @IsNotEmpty()
   name: string
 
-  @IsNotEmpty()
+  @ApiProperty({
+    example: 'web-development',
+    description: 'The slug of post type, generate with post name',
+    type: String
+  })
+  @IsNotEmpty({})
   @Validate(IsSlugVaid)
   slug: string
 
+  @ApiProperty({
+    example: 'The topic that talk about web development in general',
+    description: 'The description of post topic',
+    type: String
+  })
   description: string
 }
--- src/modules/post-types/dto/create-post-type.dto.ts
+++ src/modules/post-types/dto/create-post-type.dto.ts
@@ -3,15 +3,27 @@ import { IsEmpty, IsNotEmpty, Validate } from 'class-validator';
 import { IsSlugVaid } from 'src/common/validators/is-slug.validator';
 
 export class CreatePostTypeDto {
-  @ApiProperty()
+  @ApiProperty({
+    example: 'Mini Course',
+    description: 'The name of post type',
+    type: String
+  })
   @IsNotEmpty()
   name: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'mini-course',
+    description: 'The slug of post type, generate with post name',
+    type: String
+  })
   @IsNotEmpty()
   @Validate(IsSlugVaid)
   slug: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'The mini course you can finish in one day',
+    description: 'The description of post type',
+    type: String
+  })
   description: string;
 }
